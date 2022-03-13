b785f09140a4bfc2eb644c35a3a6e6e8df87c59a
Saumik Khan <saumik.13@gmail.com>
2022-03-13 09:05:36 -0400

    add tool to download audio tracks

---
+++ data_collection/download_mp3.py
@@ -0,0 +1,14 @@
+from urllib import request
+import pandas as pd
+
+url="tiktok-trending.csv"
+tiktok_dataset=pd.read_csv(url, on_bad_lines='skip')
+for i in range(len(tiktok_dataset)) :
+  music_url = tiktok_dataset.loc[i, "Music URL"]
+  if music_url.endswith('.mp3'): 
+      remote_url = music_url
+      local_file = 'downloadedMp3/' + str(tiktok_dataset.loc[i, "Music ID"]) + '.mp3'
+      request.urlretrieve(remote_url, local_file)
+
+
+
