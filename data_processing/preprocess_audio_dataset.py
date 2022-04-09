c4989bc37700ea4284510380f529ba36bec64175
Saumik Khan <saumik.13@gmail.com>
2022-04-09 02:20:54 -0400

    refactor code

--- data_processing/preprocess_audio_dataset.py
+++ data_processing/preprocess_audio_dataset.py
@@ -2,82 +2,94 @@
 from process_audio import create_mfcc, print_stats
 import torchaudio
 from torchaudio.transforms import Spectrogram
-import os 
+import os
 from util import get_project_root
 import math
-import json 
+import json
 
-SAMPLE_RATE = 22050 
-DURATION    = 5
+SAMPLE_RATE = 22050
+DURATION = 5
 JSON_PATH = "tiktok_audio_dataset.json"
 SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
-AUDIO_FILE_PATH = '/data_collection/downloadedMp3'
-DIRECTORY = str(get_project_root())+ AUDIO_FILE_PATH
+AUDIO_FILE_PATH = "/data_collection/downloadedMp3"
+DIRECTORY = str(get_project_root()) + AUDIO_FILE_PATH
 
-JSON_DATASET = {
-    'mfcc'          :  [], 
-    'label'         :  [],
-    'viewCount'    :  []  
-}
+JSON_DATASET = {"mfcc": [], "label": [], "viewCount": []}
 
-def build_audio_dataset(num_mfcc=13, n_fft=2048, hop_length=512, num_segments=50): 
+
+def build_audio_dataset(num_mfcc=13, n_fft=2048, hop_length=512, num_segments=50):
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    num_mfcc_vectors_per_segment = math.floor(samples_per_segment / hop_length) 
+    num_mfcc_vectors_per_segment = math.floor(samples_per_segment / hop_length)
+    print(num_mfcc_vectors_per_segment)
     tinku = True
 
     for filename in os.listdir(DIRECTORY):
         path_audio = os.path.join(DIRECTORY, filename)
-        
+
         # print(filename)
-        if str(filename).endswith('.mp3'):
+        if str(filename).endswith(".mp3"):
             view_count = str(filename).split("-")[1]
             view_count = view_count.split(".")[0]
-        else: 
+        else:
             continue
-        
-        try: 
+
+        try:
             waveform, sample_rate = torchaudio.load(path_audio)
-        except Exception: 
-            pass
-        # print(len())
-        if (waveform.dim() == 0): 
-            print(len(mfcc))
+            if len(waveform) == 0:
+                print("ERRRRORRR")
+        except Exception:
             continue
-        if len(waveform)>1: 
+        # print(len())
+        if waveform.dim() == 0:
+            print("HERE")
+            print(len(mfcc))
+            print("here")
+            continue
+        if len(waveform) > 1:
             waveform = waveform[0]
-            
-            
-        for seggs in range(num_segments):   
+
+        for seggs in range(num_segments):
             start = samples_per_segment * seggs
-            end   = start + samples_per_segment
-            mfcc_module = create_mfcc(sample_rate= sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
-            if (waveform.dim() == 0) or (len(waveform)<=0) or (len(waveform[start:end]) <= 0):
+            end = start + samples_per_segment
+            mfcc_module = create_mfcc(
+                sample_rate=sample_rate,
+                n_mfcc=num_mfcc,
+                n_fft=n_fft,
+                hop_length=hop_length,
+            )
+            if (
+                (waveform.dim() == 0)
+                or (len(waveform) <= 0)
+                or (len(waveform[start:end]) <= 0)
+            ):
                 print(len(mfcc))
-                tinku = False 
-                pass 
-            else:    
+                tinku = False
+                pass
+            else:
                 # print(len(waveform[start:end]))
-                # if not 
+                # if not
                 mfcc = mfcc_module(waveform[start:end])
                 mfcc = mfcc.T
-
-            if len(mfcc) >= num_mfcc_vectors_per_segment:
+                # print(len(mfcc))
+            if len(mfcc) == 44:
+                # print(len(mfcc))
                 JSON_DATASET["mfcc"].append(mfcc.tolist())
                 JSON_DATASET["label"].append(0)
                 JSON_DATASET["viewCount"].append(view_count)
-                if not tinku: 
+                if not tinku:
                     tinku = True
                     continue
-            
+
     with open(JSON_PATH, "w") as fp:
         json.dump(JSON_DATASET, fp, indent=6)
 
         # print_stats(waveform, sample_rate=None, src=None)
         # print((len(waveform[0])))
         # print(SAMPLE_RATE * DURATION)
-        # break 
+        # break
         # print(filename)
+
 
 if __name__ == "__main__":
     build_audio_dataset(num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5)
-    print('JSON dataset has been built!')
+    print("JSON dataset has been built!")
--- data_processing/process_audio.py
+++ data_processing/process_audio.py
@@ -3,71 +3,37 @@ import torchaudio
 import torch
 import matplotlib.pyplot as plt
 from torchaudio.transforms import Spectrogram
- 
+
+
 def mp3_to_tensor(music_id):
-   filename = 'downloadedMp3/' + music_id + '.mp3'
-   waveform, sample_rate = torchaudio.load(filename)
-   return waveform, sample_rate
- 
+    filename = "downloadedMp3/" + music_id + ".mp3"
+    waveform, sample_rate = torchaudio.load(filename)
+    return waveform, sample_rate
+
+
 def create_spectrogram(music_id, n_fft=100):
-   waveform, sample_rate = mp3_to_tensor(music_id)
-   specgram = Spectrogram(n_fft)
-   output = specgram(waveform)
-   return output
+    waveform, sample_rate = mp3_to_tensor(music_id)
+    specgram = Spectrogram(n_fft)
+    output = specgram(waveform)
+    return output
 
 
-# def create_mfcc(music_id, n_fft=100):
-#   waveform, sample_rate = mp3_to_tensor(music_id)
-#   frame_length = n_fft / sample_rate * 1000.0
-#   frame_shift = frame_length / 2.0
+def create_mfcc(sample_rate, n_mfcc, n_fft, hop_length):
+    """
+    Build mfcc for a given torchaudio waveform
 
-#   params = {
-#     "channel": 0,
-#     "dither": 0.0,
-#     "window_type": "hanning",
-#     "frame_length": frame_length,
-#     "frame_shift": frame_shift,
-#     "remove_dc_offset": False,
-#     "round_to_power_of_two": False,
-#     "sample_frequency": sample_rate,
-#   }
-#   mfcc = torchaudio.compliance.kaldi.mfcc(waveform, **params)
-#   return mfcc 
-
-def create_mfcc(sample_rate, n_mfcc, n_fft, hop_length): 
-  """
-  Build mfcc for a given torchaudio waveform 
-
-  :param signal(tensor): waveform
-  :param sample_rate (int) : ykik
-  :param n_mfcc (int) : ykik
-  :param n_fft (int) : ykik
-  :param hop_length (int) : ykik
-  """
-  mfcc = torchaudio.transforms.MFCC(
-    sample_rate=sample_rate,
-    n_mfcc=n_mfcc,
-    melkwargs={
-        "n_fft": n_fft,
-        "hop_length": hop_length,
-    },
-    
-)
-  return mfcc
-
-def print_stats(waveform, sample_rate=None, src=None):
-  if src:
-    print("-" * 10)
-    print("Source:", src)
-    print("-" * 10)
-  if sample_rate:
-    print("Sample Rate:", sample_rate)
-  print("Shape:", tuple(waveform.shape))
-  print("Dtype:", waveform.dtype)
-  print(f" - Max:     {waveform.max().item():6.3f}")
-  print(f" - Min:     {waveform.min().item():6.3f}")
-  print(f" - Mean:    {waveform.mean().item():6.3f}")
-  print(f" - Std Dev: {waveform.std().item():6.3f}")
-  print()
-  print(waveform)
-  print()
+    :param signal(tensor): waveform
+    :param sample_rate (int) : ykik
+    :param n_mfcc (int) : ykik
+    :param n_fft (int) : ykik
+    :param hop_length (int) : ykik
+    """
+    mfcc = torchaudio.transforms.MFCC(
+        sample_rate=sample_rate,
+        n_mfcc=n_mfcc,
+        melkwargs={
+            "n_fft": n_fft,
+            "hop_length": hop_length,
+        },
+    )
+    return mfcc
